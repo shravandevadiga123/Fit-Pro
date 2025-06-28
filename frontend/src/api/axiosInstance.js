5ae7a1935dacc887607e43a5cfe6a1b8e0a207a8
shravandevadiga123 <shravanlaptop06@gmail.com>
2025-06-28 17:40:06 +0530

    changed env

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5006/api",
+  baseURL: process.env.REACT_APP_API_BASE + "/api",
 });
 
 axiosInstance.interceptors.request.use(
